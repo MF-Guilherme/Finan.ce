ed40ce6230f0e4d31d91b353293b3043646ac828
Guilherme Montenegro <guimontenegro23@yahoo.com.br>
2023-07-07 21:35:14 -0300

    Atualizar categorias, listagem de contas e totais na home.

--- perfil/views.py
+++ perfil/views.py
@@ -3,18 +3,20 @@ from django.http import HttpResponse
 from .models import Conta, Categoria
 from django.contrib import messages
 from django.contrib.messages import constants
-
+from .utils import calcula_total
 
 def home(request):
-    return render(request, 'home.html')
+    contas = Conta.objects.all()
+    total_contas = calcula_total(contas, 'valor')
+    return render(request, 'home.html', {'contas': contas, 'total_contas': total_contas})
 
 
 def gerenciar(request):
     contas = Conta.objects.all()
     categorias = Categoria.objects.all()
-    total_contas = 0
-    for conta in contas:
-        total_contas += conta.valor        
+
+    total_contas = calcula_total(contas, 'valor')
+
     return render(request, 'gerenciar.html', {'contas': contas, 'total_contas': total_contas, 'categorias': categorias})
 
 
@@ -43,6 +45,7 @@ def cadastrar_banco(request):
     messages.add_message(request, constants.SUCCESS, 'Conta cadastrada com sucesso!')
     return redirect('/perfil/gerenciar')
 
+
 def deletar_banco(request, id):
     conta = Conta.objects.get(id=id)
     conta.delete()
@@ -66,4 +69,11 @@ def cadastrar_categoria(request):
     categoria.save()
 
     messages.add_message(request, constants.SUCCESS, 'Categoria cadastrada com sucesso')
+    return redirect('/perfil/gerenciar/')
+
+
+def update_categoria(request, id):
+    categoria = Categoria.objects.get(id=id)
+    categoria.essencial = not categoria.essencial
+    categoria.save()
     return redirect('/perfil/gerenciar/')
