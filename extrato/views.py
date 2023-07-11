b19c16f1a3e6b91ff52b013fe7d5c6e0a1b026c6
Guilherme Montenegro <guimontenegro23@yahoo.com.br>
2023-07-10 22:09:56 -0300

    Página para visualizar extrato criada, filtrando por conta e categoria

--- extrato/views.py
+++ extrato/views.py
@@ -4,6 +4,7 @@ from .models import Valores
 from django.contrib import messages
 from django.contrib.messages import constants
 from django.http import HttpResponse
+from datetime import datetime
 
 
 def novo_valor(request):
@@ -46,3 +47,23 @@ def novo_valor(request):
             messages.add_message(request, constants.SUCCESS, 'Saída registrada com sucesso!')
             
         return redirect('/extrato/novo_valor')
+
+
+def view_extrato(request):
+    contas = Conta.objects.all()
+    categorias = Categoria.objects.all()
+
+    conta_get = request.GET.get('conta')
+    categoria_get = request.GET.get('categoria')
+
+    valores = Valores.objects.filter(data__month=datetime.now().month)
+
+    if conta_get:
+        valores = valores.filter(conta__id=conta_get)
+
+    if categoria_get:
+        valores = valores.filter(categoria__id=categoria_get)
+
+    # TODO: botão para zerar os filtros
+    # TODO: filtrar por período
+    return render(request, 'view_extrato.html', {'valores': valores, 'contas': contas, 'categorias': categorias})
